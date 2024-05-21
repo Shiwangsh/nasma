50f6fc3362b577b04a69737e5422d05b377d9f8c
Shiwangsh KC <Shiwangshkc@gmail.com>
2024-05-21 23:56:38 +0545

    pages updated

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,5 +1,34 @@
+import { Loader } from "@/components/shared";
+import UserCard from "@/components/shared/UserCard";
+import { toast } from "@/components/ui";
+import { useGetUsers } from "@/lib/react-query/queriesAndMutations";
+
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  const { data: creators, isLoading, isError } = useGetUsers();
+  if (isError) {
+    toast({ title: "Something went wrong." });
+
+    return;
+  }
+
+  return (
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+        {isLoading && !creators ? (
+          <Loader />
+        ) : (
+          <ul className="user-grid">
+            {creators?.documents.map((creator) => (
+              <li key={creator?.$id} className="flex-1 min-w-[200px] w-full  ">
+                <UserCard user={creator} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
